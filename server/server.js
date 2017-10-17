7cf34a89a68d80a9da8c3e12fa10dd3836199eb3
tonimarintormo <antonio.marin.tormo@gmail.com>
2017-10-17 20:02:31 +0100

    Add PATCH /todos/:id

--- server/server.js
+++ server/server.js
@@ -78,7 +78,7 @@ app.patch('/todos/:id', (req, res) => {
   if(!ObjectID.isValid(id)){
     return res.status(404).send();
   }
-   console.log(body.completed);
+
   if(_.isBoolean(body.completed) && body.completed){
 
     body.completedAt = new Date().getTime();
@@ -90,14 +90,17 @@ app.patch('/todos/:id', (req, res) => {
   }
 
   Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+
     if(!todo){
       return res.status(404).send();
     }
+
     res.send({todo});
 
   }).catch((e) => {
-    console.log('6');
+
     res.status(400).send();
+    
   })
 });
 
