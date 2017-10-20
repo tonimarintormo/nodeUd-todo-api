189517ca4cffb357350cb80535fe6c734bcb2f8f
tonimarintormo <antonio.marin.tormo@gmail.com>
2017-10-20 18:06:28 +0100

    Setup separate testand development envs

--- server/server.js
+++ server/server.js
@@ -1,3 +1,5 @@
+require('./config/config');
+
 const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -8,7 +10,7 @@ var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 
 var app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 app.use(bodyParser.json());
 
@@ -100,7 +102,7 @@ app.patch('/todos/:id', (req, res) => {
   }).catch((e) => {
 
     res.status(400).send();
-    
+
   })
 });
 
